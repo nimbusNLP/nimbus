d31c151435b204d613a2b1b37072308d21d8c99a
satyapatel293 <satyapatel293@gmail.com>
2025-04-01 19:47:25 -0400

    Fix destory command to delete finished dir when completed

--- nimbus-cli/src/destroy.ts
+++ nimbus-cli/src/destroy.ts
@@ -1,9 +1,10 @@
 import { destroyStack } from "./utils/deployment.js";
+import { deleteFinishedDir } from "./utils/fileSystem.js";
 
 
 export async function destroy() {
   const currentDir = process.cwd();
   await destroyStack(currentDir);
 
-  
+  deleteFinishedDir(currentDir);  
 } 
