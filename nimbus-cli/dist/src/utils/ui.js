d6e3d70f30d8d368b1e8280bda5cb8650d01846b
Chelsoconn <c.oconnor1517@gmail.com>
2025-04-05 12:56:03 -0500

    refactored ui.ts

--- nimbus-cli/dist/src/utils/ui.js
+++ nimbus-cli/dist/src/utils/ui.js
@@ -1,85 +1,27 @@
 import figlet from "figlet";
 import chalk from "chalk";
 export function displayWelcomeMessage() {
-    // Get terminal width
-    const terminalWidth = process.stdout.columns || 80;
-    // For the title, use figlet with appropriate font size
-    const titleFont = terminalWidth < 80 ? "Small" : "Standard";
-    const asciiArt = figlet.textSync("NimbuS", {
-        font: titleFont,
-        horizontalLayout: "default",
-        verticalLayout: "default",
-    });
-    console.log(chalk.greenBright(asciiArt));
-    // For the subtitle, use plain text for small terminals
-    if (terminalWidth < 80) {
-        console.log(chalk.blue("Let's deploy your models!"));
-    }
-    else {
-        // For larger terminals, split the message to ensure words stay together
-        console.log(chalk.blue(figlet.textSync("Let's deploy", {
-            font: "Small",
-            horizontalLayout: "default",
-            verticalLayout: "default",
-        })));
-        console.log(chalk.blue(figlet.textSync("your models!", {
-            font: "Small",
-            horizontalLayout: "default",
-            verticalLayout: "default",
-        })));
-    }
+    displaySplitFiglet(chalk.greenBright, "NimbuS", undefined);
+    displaySplitFiglet(chalk.blue, "Let's deploy", "your models!");
 }
 export function displayCompletionMessage() {
-    const terminalWidth = process.stdout.columns || 80;
-    if (terminalWidth < 80) {
-        console.log(chalk.greenBright("Deployment Complete!"));
-    }
-    else {
-        console.log(chalk.greenBright(figlet.textSync("Deployment", {
-            font: "Standard",
-            horizontalLayout: "default",
-            verticalLayout: "default",
-        })));
-        console.log(chalk.greenBright(figlet.textSync("Complete!", {
-            font: "Standard",
-            horizontalLayout: "default",
-            verticalLayout: "default",
-        })));
-    }
+    displaySplitFiglet(chalk.greenBright, "Deployment", "Complete!");
 }
 export function displayDeleteWelcomeMessage() {
-    const terminalWidth = process.stdout.columns || 80;
-    if (terminalWidth < 80) {
-        console.log(chalk.greenBright("Let's delete your models!"));
-    }
-    else {
-        console.log(chalk.greenBright(figlet.textSync("Let's delete", {
-            font: "Standard",
-            horizontalLayout: "default",
-            verticalLayout: "default",
-        })));
-        console.log(chalk.greenBright(figlet.textSync("your models!", {
-            font: "Standard",
-            horizontalLayout: "default",
-            verticalLayout: "default",
-        })));
-    }
+    displaySplitFiglet(chalk.greenBright, "Let's delete", "your models!");
 }
 export function displayDeleteCompletionMessage() {
-    const terminalWidth = process.stdout.columns || 80;
-    if (terminalWidth < 80) {
-        console.log(chalk.greenBright("Deletion Complete!"));
-    }
-    else {
-        console.log(chalk.greenBright(figlet.textSync("Deletion", {
-            font: "Standard",
-            horizontalLayout: "default",
-            verticalLayout: "default",
-        })));
-        console.log(chalk.greenBright(figlet.textSync("Complete!", {
-            font: "Standard",
-            horizontalLayout: "default",
-            verticalLayout: "default",
-        })));
-    }
+    displaySplitFiglet(chalk.greenBright, "Deletion", "Complete!");
+}
+function displaySplitFiglet(colorMeth, firstPart, secondPart) {
+    console.log(colorMeth(figlet.textSync(firstPart, {
+        font: "Standard",
+        horizontalLayout: "default",
+        verticalLayout: "default",
+    })));
+    secondPart && console.log(colorMeth(figlet.textSync(secondPart, {
+        font: "Standard",
+        horizontalLayout: "default",
+        verticalLayout: "default",
+    })));
 }
