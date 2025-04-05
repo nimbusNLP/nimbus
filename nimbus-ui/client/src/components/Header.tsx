f42d33c9e4a6949a616aa1448899d4862b5a4b12
Richard LoRicco <rtlo9962@gmail.com>
2025-04-05 13:38:57 -0400

    setup tailwind v3

--- nimbus-ui/client/src/components/Header.tsx
+++ nimbus-ui/client/src/components/Header.tsx
@@ -1,7 +1,11 @@
 const Header = () => {
   return (
-    <h1>Nimbus *Cloud*</h1> 
-  )
-}
+    <header className="bg-blue-600 text-white p-4 shadow-md">
+      <h1 className="text-2xl text-green-200 font-bold text-center">
+        Nimbus <span className="text-blue-200">Cloud</span>
+      </h1>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
